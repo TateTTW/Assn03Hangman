8079ed94c06bc5b7c18852bffaf5b4722afa2289
tatet <tatet@192.168.1.236>
2019-10-02 19:55:16 -0400

    rebuild game to the specifications

--- js/hangman.js
+++ js/hangman.js
@@ -1,35 +1,115 @@
-var words = ["rock","paper","scissors","airplane","cat","car","tree","dog","house","book", "foot" , "firepit", "police"
-	,"keyboard","computer","building","road","train","desk","neighbor","picture","boat","elephant","frame","game","motorcycle",
-	"printer","light","pillow","bed","arcade","window","hill","grass","garage","weight","office","stapler","couch","monkey",
-	"cup","bottle","carpet","wood","ship","sky","father","mother","brother","sister","aunt","uncle","cousin","bridge","stone",
-	"rug","vacuum", "boy", "girl", "europe" , "america", "canada", "russia", "africa", "asia", "japan", "antarctica", "alaska",
-	"bike","mountain", "ocean", "sea", "continent", "trade", "school", "class", "student", "teacher", "science", "art", "human",
-	"read", "predator", "religion", "idiot", "money", "phone", "watch", "electricity", "wire", "gym", "fun", "heavy", "dolphin"];
 var guesses = [];
+var misses = 0;
 
 function getRandomWord(){
-	var num = Math.floor(Math.random() * ((words.length-1) - 0 + 0)) + 0;
-	return words[num];
+	var lvl = document.getElementById('lvlSelect');
+	var url =  'https://hangman-api.lively.software';
+	var word = '';
+	
+	if(lvl != undefined && lvl.value != ''){
+		url+= '?difficulty='+lvl.value;
+	}
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       var result = JSON.parse(xhttp.responseText);
+	       buildWord(result.word);
+	    }
+	};
+	xhttp.open("GET", url , true);
+	xhttp.send();
 }
 
 function start(){
-	this.word = getRandomWord();
+	clearCanvas();
 	clearGuesses();
-	hideHangman();
-	buildRandomWord();
+	getRandomWord();
 }
 
-function clearWordDisplay(){
+function clearCanvas(){
+	misses = 0;
+	var canvas = document.querySelector('canvas');
+	canvas.height = 400;
+	canvas.width = 400;
+	var c = canvas.getContext('2d');
+	c.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawHangman(){
+	var canvas = document.querySelector('canvas');
+	canvas.height = 400;
+	canvas.width = 400;
+	var c = canvas.getContext('2d');
+	c.lineWidth = 3;
+	c.strokeStyle = "black";
+	misses++;
+	
+	switch(misses){
+	case 9:	
+		//right leg
+		c.beginPath();
+		c.moveTo(200, 270);
+		c.lineTo(255, 360)
+		c.stroke();
+	case 8:	
+		//left leg
+		c.beginPath();
+		c.moveTo(200, 270);
+		c.lineTo(145, 360)
+		c.stroke();
+	case 7:	
+		//right arm
+		c.beginPath();
+		c.moveTo(200, 210);
+		c.lineTo(290, 150);
+		c.stroke();
+	case 6:		
+		//left arm
+		c.beginPath();
+		c.moveTo(110, 150);
+		c.lineTo(200, 210);
+		c.stroke();
+	case 5:	
+		//body
+		c.beginPath();
+		c.moveTo(200, 170);
+		c.lineTo(200, 270);
+		c.stroke();	
+		//noose
+		c.fillStyle = "#804000";
+		c.fillRect(197,170,10,5);
+	case 4:	
+		//head
+		c.beginPath();
+		c.arc(200,140,30,0,Math.PI * 2,false)
+		c.stroke();		
+		//noose
+		c.fillStyle = "#804000";
+		c.fillRect(197,170,10,5);
+	case 3:	
+		//rope
+		c.fillStyle = "#804000";
+		c.fillRect(200,50,9,60);
+	case 2:	
+		//branch
+		c.fillStyle = "#663300";
+		c.fillRect(95,50,270,15);
+	case 1:
+		//pole
+		c.fillStyle = "#663300";
+		c.fillRect(350,50,20,400);
+		break;
+	default:
+	}
+	
+}
+
+function buildWord(word){
+	this.word = word;
 	var wordDisplay = document.getElementById('randomWord');
 	wordDisplay.innerHTML = '';
-}
-
-function buildRandomWord(){
-	clearWordDisplay();
-	var wordDisplay = document.getElementById('randomWord');
-	var str = '';
-	for(var i = 0; i < this.word.length ; i++){
-		wordDisplay.insertAdjacentHTML('beforeend','<span class="letterPlaceholder"><span class="letter" style="visibility: hidden">'+ this.word.charAt(i) +'</span></span>');
+	for(var i = 0; i < word.length ; i++){
+		wordDisplay.insertAdjacentHTML('beforeend','<span class="letterPlaceholder"><span class="letter" style="visibility: hidden">'+ word.charAt(i) +'</span></span>');
 	}
 }
 
@@ -62,7 +142,13 @@ function processKeyPress(key){
 			start();
 		}});
 	} else {
-		showAnImage();
+		drawHangman();
+		if(hasLost()){
+			setTimeout(function(){
+				alert('You Lose! The word was ' + this.word + '.');
+				start();
+			});
+		}
 	}
 	
 }
@@ -83,40 +169,8 @@ function clearGuesses(){
 	prevLetters.innerHTML = '';
 }
 
-function showAnImage(){
-	var imgs = document.getElementsByTagName("img");
-	for(var i = 0; i < imgs.length ; i++){
-		if(imgs[i].style.visibility === 'hidden'){
-			if(imgs[i].id === 'leftArm' && imgs[i+1].style.visibility === 'hidden'){
-				imgs[i+1].style.visibility = 'visible';
-			} else {
-				imgs[i].style.visibility = 'visible';
-			}
-			setTimeout(function(){
-				if(hasLost()){
-					alert('You Lose! The word was ' + this.word + '.');
-					start();
-			}});
-			break;
-		}
-	}
-}
-
 function hasLost(){
-	var imgs = document.getElementsByTagName("img");
-	for(var i = 0; i < imgs.length ; i++){
-		if(imgs[i].style.visibility === 'hidden'){
-			return false;
-		}
-	}
-	return true;
-}
-
-function hideHangman(){
-	var imgs = document.getElementsByTagName("img");
-	for(var i = 0; i < imgs.length ; i++){
-		imgs[i].style.visibility = 'hidden';
-	}
+	return misses >= 9;
 }
 
 function hasWon(){
